8f62f4e16bc724a52342925aced845bfe89530e1
Yonatan Snir <yonatans@radware.com>
2024-06-09 12:13:49 +0300

    decrement vite-tsconfig-path version

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from "react";
 // import { Header } from "./layout/Header/Header";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
-import * as Style from "./layout/AppWrapper.style";
-import { Menu } from "./layout/Menu/Menu";
-import { SubMenu } from "./layout/Menu/SubMenu";
+import * as Style from "layout/AppWrapper.style";
+import { Menu } from "layout/Menu/Menu";
+import { SubMenu } from "layout/Menu/SubMenu";
 
 const App = () => {
   return (
