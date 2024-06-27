8a3761515757a04af9ca5619db2c2912c10f0836
Yodana <yodana@radware.com>
2024-06-28 01:07:05 +0300

    404 img

--- src/frontend/pages/NotFound/NotFound.style.ts
+++ src/frontend/pages/NotFound/NotFound.style.ts
@@ -1,30 +1,30 @@
-import styled, {
-  keyframes,
-} from "styled-components";
-
-const blinker = keyframes`
-  30% {
-    opacity: 0.3;
-  }
-`;
+import styled from "styled-components";
 
 export const PageWrap = styled.div`
-  width: 100%;
   height: 100%;
   display: flex;
+  align-items: center;
+`;
+
+export const ContentWrap = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 35px;
   justify-content: center;
   align-items: center;
-  flex-direction: column;
 `;
 
 export const Number = styled.div`
-  font-size: 150px;
+  font-size: 100px;
   font-family: monospace;
-  line-height: 130px;
-  animation: ${blinker} 2s linear infinite;
+  line-height: 100px;
+  text-align: center;
+  color: #fdfff0;
 `;
 
 export const Text = styled.div`
   font-size: 35px;
   font-family: monospace;
+  color: #fdfff0;
 `;
--- src/frontend/pages/NotFound/NotFound.tsx
+++ src/frontend/pages/NotFound/NotFound.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import {
-  PageWrap,
+  ContentWrap,
   Number,
   Text,
+  PageWrap,
 } from "./NotFound.style";
 
 export const NotFound = () => {
   return (
     <PageWrap>
-      <Number>404</Number>
-      <Text>Page Not Found</Text>
+      <ContentWrap>
+        <img width={300} src="/imgs/404.png" />
+        <div>
+          <Number>404</Number>
+          <Text>Page Not Found</Text>
+        </div>
+      </ContentWrap>
     </PageWrap>
   );
 };
